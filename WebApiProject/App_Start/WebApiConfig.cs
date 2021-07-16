b3098e8ccc2c9f8510e74fd5c4c982b6a5e7c241
saraorkari <87174453+saraorkari@users.noreply.github.com>
2021-07-16 11:02:22 +0300

    עדכון C#

--- WebApiProject/App_Start/WebApiConfig.cs
+++ WebApiProject/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace WebApiProject
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
